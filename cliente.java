f4b2a05532e2d83b45bd9ce310d1ee0d5325c77b
MathhewEastwood <142929263+MathhewEastwood@users.noreply.github.com>
2023-08-31 12:38:13 -0500

    Add files via upload

---
+++ cliente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personas;
+
+/**
+ *
+ * @author SALA
+ */
+
+public class cliente {
+    private String nombre;
+    String Getnombre()
+    {
+        return nombre;
+        
+    }
+    public void setNombre(String nombre)
+    {
+        this.nombre=nombre;
+    }
+}
